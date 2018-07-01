357df5b70f0f807e9f7d62be75c57e901849ae50
Derick <agbemadud@gmail.com>
2018-07-01 20:27:41 +0100

    final commit

--- js/currencyconvertor.js
+++ js/currencyconvertor.js
@@ -11,9 +11,7 @@ var dbPromise = idb.open('currency-convertor', 1, (upgradeDb) => {
     var keyValStore = tx.objectStore('conversion-rates');
     keyValStore.put(val, key);
     return tx.complete;
-  }).then(()=> {
-    console.log('Added foo:bar to conversion-rates');
-  });   
+  });
   }
 let amount = document.querySelector('#amount').innerHTML;
 const select = document.querySelector('#myCurrency');
@@ -21,13 +19,14 @@ const selects = document.querySelector('#myCurrenci');
 let currenciesUrl = 'https://free.currencyconverterapi.com/api/v5/currencies';
 
 fetch(currenciesUrl).then(response =>{
-    console.log(response)
     if (response.status !== 200) {
      console.warn('your request did not go because of' + response.status)
     }else{
         response.json().then(results =>{
             for (const myResult in results){
+                console.log(Object.entries(results)[0]);
                 for (const id in results[myResult]){
+                    console.log(id);
                     let option = document.createElement('option');
                     option.value = id;
                     option.text = id;
@@ -48,14 +47,6 @@ fetch(currenciesUrl).then(response =>{
 })
 
 
-
-
-
-
-
-
-
-
 document.querySelector("#btnConvert").addEventListener("click", convertCurrency);
 
 function convertCurrency() {
@@ -63,7 +54,6 @@ function convertCurrency() {
     let selects = document.querySelector('#myCurrenci');
     let fromCurrency = select.value
     let toCurrency = selects.value
-    console.log(fromCurrency,toCurrency)
     fromCurrency = encodeURIComponent(fromCurrency);
     toCurrency = encodeURIComponent(toCurrency);
     const query = fromCurrency + '_' + toCurrency;
@@ -76,30 +66,25 @@ function convertCurrency() {
         return keyValStore.get(query);
     })
     .then((val)=> {
-        console.log('The value of "hello" is:', val);
-    if (val != null){
-    conversionRate = val
-    let userInput = document.getElementById("amount").value;
-  let total = conversionRate * userInput;
-  let actualConversion = Math.round(total * 100)/100;
-  console.log(actualConversion)
-  document.getElementById('amountResult').innerHTML = `${toCurrency} ${actualConversion}`;
+            if (val != null){
+            conversionRate = val
+            let userInput = document.getElementById("amount").value;
+            let total = conversionRate * userInput;
+            let actualConversion = Math.round(total * 100)/100;
+            document.getElementById('amountResult').innerHTML = `${toCurrency} ${actualConversion}`;
     }else{
     if(navigator.onLine == true){
-       let url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
-     fetch(url).then((response)=>{
-  return response.json().then((results)=>{
- console.log(results, query)
- conversionRate = results[query];
-  console.log(conversionRate)
-  //set's the conversion rate to the indexedDB
-  setVal(query,conversionRate)
-  let userInput = document.getElementById("amount").value;
-  let total = conversionRate * userInput;
-  let actualConversion = Math.round(total * 100)/100;
-  console.log(actualConversion)
-  document.getElementById('amountResult').innerHTML = `${toCurrency} ${actualConversion}`;
-  })
+             let url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
+                fetch(url).then((response)=>{
+                return response.json().then((results)=>{
+                conversionRate = results[query];
+                //set's the conversion rate to the indexedDB
+                setVal(query,conversionRate)
+                let userInput = document.getElementById("amount").value;
+                let total = conversionRate * userInput;
+                let actualConversion = Math.round(total * 100)/100;
+                document.getElementById('amountResult').innerHTML = `${toCurrency} ${actualConversion}`;
+    })
   });   
     }else{
         alert('Looks like this exchange rate is not available offline. Get connectivity and try again')
